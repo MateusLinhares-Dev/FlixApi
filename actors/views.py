4965dff01b7f5c9ba820dcdbe138c56d7f677be6
Mateus Epifanio Linhares <83597337+MateusLinhares-Dev@users.noreply.github.com>
2024-05-20 13:32:34 -0300

    actors

--- actors/views.py
+++ actors/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
-
-# Create your views here.
+from rest_framework import generics
+from actors.models import Actor
+from actors.serializers import SerializerActorModel
+#Lista e criar
+class ActorCreateListView(generics.ListCreateAPIView):
+    queryset = Actor.objects.all()
+    serializer_class = SerializerActorModel
+    
+# detail, delete and put
+class ActorUpdateRetrieveDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Actor.objects.all()
+    serializer_class = SerializerActorModel
--- app/urls.py
+++ app/urls.py
@@ -1,11 +1,15 @@
 from django.contrib import admin
 from django.urls import path
 from genres.views import GenreCreateListView, GenereRetrieveUpdateDestroyView
-
+from actors.views import ActorCreateListView, ActorUpdateRetrieveDestroyView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('genres/', GenreCreateListView.as_view(), name='genre_creat_list'),
     #passar o id da tabela para retornar o dado especificado.
     path('genres/<int:pk>', GenereRetrieveUpdateDestroyView.as_view(), name='genre_detail_view'),
+
+    path('actors/', ActorCreateListView.as_view(), name='actor_detail_view'),
+    #passar o id da tabela para retornar o dado especificado.
+    path('actors/<int:pk>', ActorUpdateRetrieveDestroyView.as_view(), name='actor_create_view')
 ]
