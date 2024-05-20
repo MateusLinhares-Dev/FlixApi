48a53793c112520be39189e41e19e15481629e05
Mateus Epifanio Linhares <83597337+MateusLinhares-Dev@users.noreply.github.com>
2024-05-20 10:21:58 -0300

    create DRF and setting url

--- app/urls.py
+++ app/urls.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from django.urls import path
+from genres.views import GenreCreateListView, GenereRetrieveUpdateDestroyView
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('genres', GenreCreateListView.as_view(), name='genre_creat_list'),
+    #passar o id da tabela para retornar o dado especificado.
+    path('genres/<int:pk>', GenereRetrieveUpdateDestroyView.as_view(), name='genre_detail_view'),
 ]
--- genres/views.py
+++ genres/views.py
@@ -1,10 +1,15 @@
-from django.shortcuts import render
 #generics do DRF
 from rest_framework import generics
-#import status code
-from django.shortcuts import get_object_or_404
-#json
-from django.http import JsonResponse
 #Models
 from genres.models import Genre
+#serializer
+from genres.serializers import GenreSerializer
 
+class GenreCreateListView(generics.ListCreateAPIView):
+    queryset = Genre.objects.all()
+    #serializador importado
+    serializer_class = GenreSerializer
+
+class GenereRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
