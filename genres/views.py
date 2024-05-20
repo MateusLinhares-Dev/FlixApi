bb384fc7f59c9abc451f64bd6e6fd041261147f5
Mateus Epifanio Linhares <83597337+MateusLinhares-Dev@users.noreply.github.com>
2024-05-20 08:55:25 -0300

    update models, import views

--- genres/views.py
+++ genres/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+#generics do DRF
+from rest_framework import generics
+#import status code
+from django.shortcuts import get_object_or_404
+#json
+from django.http import JsonResponse
+#Models
+from genres.models import Genre
 
-# Create your views here.
