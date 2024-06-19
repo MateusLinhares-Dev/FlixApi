1418d858ed76d2f26305a0c859b5b5cb792abc97
Mateus Epifanio Linhares <83597337+MateusLinhares-Dev@users.noreply.github.com>
2024-06-19 16:53:09 -0300

    update

--- app/settings.py
+++ app/settings.py
@@ -1,3 +1,4 @@
+from datetime import timedelta
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -123,4 +124,9 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     )
+}
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME":timedelta(days=1),
+    "REFRESH_TOKEN_LIFETIME":timedelta(days=7),
 }
